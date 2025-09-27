ad98ba0a86c1ed06ec3ebfd0edf6eefa339a7ec4
Artem <tema25062010@gmail.com>
2025-09-28 00:45:33 +0300

    Implementing the functionality of the application using the properties of the components and introducing a small data database to understand how to form new employees based on this

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,12 +1,17 @@
 import EmployeesListItem from "../employers-list-item/employers-list-item";
 import './employers-list.css'
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        return (
+            <EmployeesListItem {...item}/>
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
