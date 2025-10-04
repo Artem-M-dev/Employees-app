437f76cc482f0c8d1568e7e418f2258591863b9a
Artem <tema25062010@gmail.com>
2025-10-04 13:12:30 +0300

    create new function(method) delete item which remove an employee when the red button(trash) is pressed. Also we create a new attribute for EmployersList onDelete which use function this.deleteItem inside yourself

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -24,7 +24,7 @@ class EmployeesListItem extends Component {
     }
 
     render() {
-        const {name, salary} = this.props;
+        const {name, salary, onDelete} = this.props;
         const {increase, star} = this.state;
         
         let classNames = "list-group-item d-flex justify-content-between"
@@ -42,12 +42,13 @@ class EmployeesListItem extends Component {
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
                         className="btn-cookie btn-sm "
-                        onClick={this.onIncrease}>
+                        onClick={this.onIncrease}>  
                         <i className="fas fa-cookie"></i>
                     </button>
 
                     <button type="button"
-                        className="btn-trash btn-sm ">
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}>
                         <i className="fas fa-trash"></i>
                     </button>
                     <i className="fas fa-star"></i>
