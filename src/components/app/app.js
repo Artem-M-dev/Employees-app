639ba86465cd4d66479712c329ef1407f599ea59
Artem <tema25062010@gmail.com>
2025-10-05 11:10:03 +0300

    create new function which add a new employee after submit form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,12 +17,28 @@ class App extends Component {
                 { name: 'Carl W.', salary: 5000, increase: false, id: 3 }
             ]
         }
+        this.maxId = 3
     }
 
     deleteItem = (id) => {
         this.setState(({data}) => {
             return {
                 data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name: name, 
+            salary: salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem]
+            return {
+                data: newArr
             }
         })
     }
@@ -40,7 +56,7 @@ class App extends Component {
                 <EmployersList
                     data={this.state.data}
                     onDelete={this.deleteItem} />
-                <EmployeesAddForm />
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         )
     }
--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -7,7 +7,7 @@ class EmployeesAddForm extends Component {
         super(props)
         this.state = {
             name: '',
-            salary: 0
+            salary: ''
         }
     }
 
@@ -17,15 +17,26 @@ class EmployeesAddForm extends Component {
             [e.target.name]: e.target.value
         })
     }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: '',
+        })
+    }
     
     render() {
         const {salary, name} = this.state
+        // const {addItem} = this.props
 
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
@@ -39,7 +50,8 @@ class EmployeesAddForm extends Component {
                         value={salary} 
                         onChange={this.onValueChange}/>
 
-                    <button type="submit"
+                    <button 
+                        type="submit"
                         className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
